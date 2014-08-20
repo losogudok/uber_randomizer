476c06c5f94b3d0cf61ec65ab8b60b98c25635ee
andrey <losogudok@yandex.ru>
2014-08-20 08:26:26 +0400

    Add server and api

--- scripts.js
+++ scripts.js
@@ -1,28 +1,69 @@
 (function(document, window){
 
-var btn = document.querySelector('btnEat');
+
+const CLIENT_ID = "BTQYSYTNCW45PVNFUXYRXPCJYOYA3U4LRO5HHL2CBDX3DVMX";
+const CLIENT_SECRET = "KEJ5US3VF5H5T0ZKPKECKFQURD1AUVGKGTL5SRCAT5T44ZGT"
+const VERSION = "20140818";
+const URL = "https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+
+			"&client_secret="+CLIENT_SECRET+"&v="+VERSION;
+
+var btn = document.querySelector('#btnEat');
+var circle = document.querySelector('.circle');
+var result = document.querySelector('.result');
 var location;
 
-
-
 // Listeners
-btn.addEventListener('click', makeRandom, true);
+// btn.addEventListener('click', makeRandom, true);
+circle.addEventListener('touchend', makeRandom, true);
 
-
-// Handler
+// Handlers
 function makeRandom() {
-	getLocation()
+	console.log('randomizing!');
+	getCurrentLocation()
+		.then(performAjaxRequest, null)
+		.then(showResult);
 }
 
-function getLocation() {
+function getCurrentLocation() {
 	var promise = new Promise(function(resolve, reject){
 		navigator.geolocation.getCurrentPosition(function(position) {
-		  	resolve({latitude: position.coords.latitude, longitude: position.coords.longitude});
+			var latitude = String(position.coords.latitude).slice(0, 4);
+			var longitude = String(position.coords.longitude).slice(0, 4);
+			console.log(latitude, longitude);
+		  	resolve({latitude: latitude, longitude: longitude });
 		},
 		function(error){
 			reject(error);
 		});
 	});
+	return promise;
 }
 
-})(document, window)
+function performAjaxRequest(pos) {
+	var promise = new Promise(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		var query = "&ll="+pos.latitude+","+pos.longitude+"&radius=1000&query=кафе";
+		var url = URL + query;
+		console.log(url);
+		xhr.open("GET", url, true);
+		xhr.onload = function() {
+			if (this.status === 200) {
+				resolve(this.responseText);
+			}
+			else {
+				reject(this.statusText);
+			}
+		}
+		xhr.send(null);
+	});
+
+	return promise;
+}
+
+function showResult(data) {
+	btn.classList.add('hide');
+	result.classList.add('show');
+	result.innerHTML = data;
+}
+
+})(document, window);
---
+++ server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+	
+	switch (req.url) {
+		case '/':		
+			res.setHeader("Content-Type", "text/html");
+			fs.createReadStream('index.html').pipe(res);
+			break;
+		case '/styles.css':
+			res.setHeader("Content-Type", "text/css");
+			fs.createReadStream('styles.css').pipe(res);	
+			break;
+		case '/scripts.js':
+			res.setHeader("Content-Type", "application/javascript");
+			fs.createReadStream('scripts.js').pipe(res);	
+	}
+	
+});
+
+server.listen(3000, function(){
+	console.log('Server listening om port ' + 3000);
+});
