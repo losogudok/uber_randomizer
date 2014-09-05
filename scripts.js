f0f0ec7991ed601091711d8ac1d6f6cb2fca16f7
andrey <losogudok@yandex.ru>
2014-09-05 10:00:03 +0400

    New features

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ const URL = "https://api.foursquare.com/v2/venues/explore?client_id="+CLIENT_ID+
 var btn = document.querySelector('#btnEat');
 var circle = document.querySelector('.circle');
 var result = document.querySelector('.result');
+var name = result.querySelector('.venue-name');
+var address = result.querySelector('.venue-address');
 var location;
 
 // Listeners
@@ -21,19 +23,25 @@ function makeRandom() {
 	console.log('randomizing!');
 	getCurrentLocation()
 		.then(performAjaxRequest, null)
-		.then(showResult);
+		.then(function(data){
+			var places = prepareJSON(data);
+			var place = pickRandomPlace(places);
+			showResult(place);
+		});
 }
 
 function getCurrentLocation() {
 	var promise = new Promise(function(resolve, reject){
 		navigator.geolocation.getCurrentPosition(function(position) {
-			var latitude = String(position.coords.latitude).slice(0, 4);
-			var longitude = String(position.coords.longitude).slice(0, 4);
+			var latitude = String(position.coords.latitude).slice(0, 6);
+			var longitude = String(position.coords.longitude).slice(0, 6);
 			console.log(latitude, longitude);
 		  	resolve({latitude: latitude, longitude: longitude });
 		},
 		function(error){
 			reject(error);
+		}, {
+			enableHighAccuracy: true
 		});
 	});
 	return promise;
@@ -42,7 +50,7 @@ function getCurrentLocation() {
 function performAjaxRequest(pos) {
 	var promise = new Promise(function(resolve, reject){
 		var xhr = new XMLHttpRequest();
-		var query = "&ll="+pos.latitude+","+pos.longitude+"&radius=1000&query=кафе";
+		var query = "&ll="+pos.latitude+","+pos.longitude+"&llAcc=10&limit=50&radius=1000&query=кафе";
 		var url = URL + query;
 		console.log(url);
 		xhr.open("GET", url, true);
@@ -59,11 +67,27 @@ function performAjaxRequest(pos) {
 
 	return promise;
 }
+function prepareJSON(data) {
+	var dataObj = JSON.parse(data);
+	var filtered = dataObj.response.groups[0].items.filter(function(el, i){
+		if (el.flags && el.flags.outsideRadius) {
+			return false;
+		}
+		return true;
+	});
+	return filtered;
+}
+function pickRandomPlace(places) {
+	var i = Math.floor((Math.random() * places.length));
+	return places[i];
+}
 
 function showResult(data) {
-	btn.classList.add('hide');
+	circle.classList.add('hide');
 	result.classList.add('show');
-	result.innerHTML = data;
+	var venue = data.venue;
+	name.textContent = venue.name;
+	address.textContent = venue.location.address;
 }
 
 })(document, window);
